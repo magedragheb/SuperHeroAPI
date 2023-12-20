b3215d0d9b3ea173311a93684b5e208bf7ce5324
Maged <19859654+magedragheb@users.noreply.github.com>
2023-12-20 20:38:38 +0200

    fix inheritance from IdentityDbContext

--- SuperHeroAPI/Entities/HeroContext.cs
+++ SuperHeroAPI/Entities/HeroContext.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 namespace SuperHeroAPI.Entities;
 
-public class HeroContext(DbContextOptions<HeroContext> options) : IdentityDbContext(options)
+public class HeroContext(DbContextOptions<HeroContext> options) : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<AppUser> AppUsers => Set<AppUser>();
     public DbSet<Hero> Heroes => Set<Hero>();
