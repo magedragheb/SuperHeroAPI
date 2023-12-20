c17755afb3a6f04d104966119ade9dcd3376abf6
Maged <19859654+magedragheb@users.noreply.github.com>
2023-12-20 18:06:32 +0200

    register dbcntext and httpclient

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<HeroContext>(opt => opt
+                .UseSqlite("Data Source=Data/SuperHeroDB"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
