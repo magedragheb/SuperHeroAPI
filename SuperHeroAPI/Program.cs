6a14888d738a1b03cda0ed5e2e951f74a9172e50
Maged <19859654+magedragheb@users.noreply.github.com>
2023-12-20 20:07:22 +0200

    add swagger documentation

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SuperHeroAPI.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,25 @@ builder.Services.AddDbContext<HeroContext>(opt => opt
                 .UseSqlite("Data Source=Data/SuperHeroDB"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddHttpClient();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SuperHeroAPI",
+        Version = "v1",
+        Description = "Reading heroes from https://superheroapi.com/ and adding to favourites per user"
+    });
+});
 
 var app = builder.Build();
-
+app.Urls.Add("https://localhost:5000");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
